93f3cfe8780f1c92a7761b8822bb156144a92e97
Wei-xu293 <falseacc007@gmail.com>
2025-10-21 23:19:23 +0200

    dsd

---
+++ lab/src/X13379.clj
@@ -0,0 +1,13 @@
+(ns X13379)
+
+;En aquest problema heu d’implementar una sèrie de funcions en 
+;Clojure utilitzant només funcions d’ordres superior.
+
+;Feu una funció mides que prengui una llista d’strings, 
+;calculi la longitud de cada cadena, elimini les longituds parells i sumi les longituds restants.
+;Feu una funció noms que, donat un map de noms i edats,
+;retorni la llista dels noms de les persones majors de 18 anys, ordenades alfabèticament.
+;Feu una funció suma que, donat un vector de maps amb 
+;les claus :preu i :quantitat, calculi el total per cada element (preu * quantitat), elimini els que tinguin un valor total inferior a 100 i sumi tots els totals.
+
+(defn mides [ls] ())
