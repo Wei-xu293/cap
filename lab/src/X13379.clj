70d4953e0e8ab9da1765fd860bda8ffbf166c0fb
Wei-xu293 <falseacc007@gmail.com>
2025-10-27 23:08:01 +0100

    X13379

--- lab/src/X13379.clj
+++ lab/src/X13379.clj
@@ -1,13 +1,12 @@
 (ns X13379)
 
-;En aquest problema heu d’implementar una sèrie de funcions en 
-;Clojure utilitzant només funcions d’ordres superior.
+(defn mides [ls] 
+  (apply + (filter odd? (map count ls))))
 
-;Feu una funció mides que prengui una llista d’strings, 
-;calculi la longitud de cada cadena, elimini les longituds parells i sumi les longituds restants.
-;Feu una funció noms que, donat un map de noms i edats,
-;retorni la llista dels noms de les persones majors de 18 anys, ordenades alfabèticament.
-;Feu una funció suma que, donat un vector de maps amb 
-;les claus :preu i :quantitat, calculi el total per cada element (preu * quantitat), elimini els que tinguin un valor total inferior a 100 i sumi tots els totals.
+(defn getSnd [name age] )
 
-(defn mides [ls] ())
+(defn noms [ls]
+  (sort (map (comp symbol key) (filter #(> (val %) 18) ls))))
+
+(defn suma [mp]
+  (reduce + (filter #(>= % 100) (map #(* (:quantitat %) (:preu %)) mp))))
