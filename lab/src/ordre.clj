9e6a6301fce79e2b4acb596d6af0d253dc4a8953
Wei-xu293 <falseacc007@gmail.com>
2025-10-28 22:53:58 +0100

    ordre

--- lab/src/ordre.clj
+++ lab/src/ordre.clj
@@ -4,7 +4,7 @@
   [fs]
   (letfn [(g [x y] 
              (println ((first fs) x y)) 
-             (ordre (conj (vec (rest fs)) #((first fs) %1 %2))))]
+             (ordre (conj (vec (rest fs)) (first fs))))]
     g))
 
 (def funcions [+ * -]) 
