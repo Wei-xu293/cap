5fe4bdbc9a69d3416e0b5c9a7c32180a5db082ef
Wei-xu293 <falseacc007@gmail.com>
2025-11-02 15:12:26 +0100

    aplica

--- lab/src/aplica.clj
+++ lab/src/aplica.clj
@@ -2,12 +2,17 @@
 
 (require '[clojure.math :as m])
 
-(defn aplica
-  [f]
-  (letfn [(g [x]
-             (when (== x -1) (println (f x)))
-             (conj [] (aplica f)))]
-    g))
+(defn aplica [f]
+  (letfn [(g [acc] 
+             (letfn [(apl [x]
+                     (assert (or (= -1 x) (pos? x)))
+                     (if (= -1 x) (println acc)
+                         (g (conj acc (f x)))))]
+               apl))] 
+    (g [])))
 
 (def h (aplica m/sqrt))
 ((((((h 2) 4) 6) 8) 10) -1)
+
+(def h (aplica m/cos))
+((((h (/ m/PI 4)) (/ m/PI 2)) m/PI) -1)
