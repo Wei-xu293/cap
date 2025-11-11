5cb0715f4fe970540534bb88a62b0588a71014e0
Wei-xu293 <falseacc007@gmail.com>
2025-11-11 12:10:59 +0100

    misteri

--- lab/src/misteri.clj
+++ lab/src/misteri.clj
@@ -1,12 +1,14 @@
 (ns misteri)
 (defn misteri [n]
   (let [secret 4
-        n (+ n 2)]
+        n (+ n 2)] ; n = 5
     (fn [mult]
-      (* secret (* mult n)))))
-(defn misteri3 [param]
+      (* secret (* mult n))))) ; 4 * 6 * 5 = 120
+
+(defn misteri3 [param] ; param = misteri 3 = h
   (fn [bonus]
-    (+ (param 6) bonus)))
+    (+ (param 6) bonus))) ; (param 6) = misteri 3 (mult 6) = 120 + 2 = 122
+
 (let [h (misteri 3)
       j (misteri3 h)
       result (j 2)]
