d3d5fbfd3635fc993097aec3d617d6d3977fc518
Wei-xu293 <falseacc007@gmail.com>
2025-11-13 15:57:42 +0100

    punts

--- lab/src/punts2.clj
+++ lab/src/punts2.clj
@@ -1,3 +1,6 @@
 (ns punts2)
 
-(def punt (list (atom 0) (atom 0)))
+(defn punt [x y] (let [px (atom 0) py (atom 0)]
+  (letfn [(f [c & p]
+    (cond (= c :crt) (list (reset! px x) (reset! py y)) ))]
+    f)))
