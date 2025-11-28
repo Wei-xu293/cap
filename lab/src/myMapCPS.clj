03a1aa10f12b449f0afb9cc682e35c874486d34e
Wei-xu293 <falseacc007@gmail.com>
2025-11-28 13:05:05 +0100

    trampoline

--- lab/src/myMapCPS.clj
+++ lab/src/myMapCPS.clj
@@ -10,4 +10,17 @@
 (defn my-map-cps [f lst cont]
   (if (empty? lst) (cont lst)
        (let [[cap & cua] lst]
-         (my-map-cps f cua (fn [x] (cont (cons (f cap) x)))))))
+         (recur f cua (fn [x] (cont (cons (f cap) x)))))))
+
+
+;; (trampoline my-map-cps-t inc (range 100000) identity)
+
+(defn my-map-cps-t [f lst k]
+  (if (empty? lst)
+    #(k '())
+    (let [[cap & cua] lst]
+      #(my-map-cps-t 
+        f 
+        cua
+        (fn [mapped-tail]
+          (fn [] (k (cons (f cap) mapped-tail))))))))
