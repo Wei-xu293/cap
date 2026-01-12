530f23f09a50b995218e73ed68684e721352a429
Wei-xu293 <falseacc007@gmail.com>
2026-01-12 13:43:27 +0100

    seq lazy

--- lab/src/seq_lazy.clj
+++ lab/src/seq_lazy.clj
@@ -1,2 +1,7 @@
-(ns seq-lazy)
+(def naturals (lazy-seq (cons 0 (map inc naturals))))
+(def factorials (lazy-seq (cons 1N (map * factorials (iterate inc 1N)))))
+
+(def f identity)
+
+(def seq-general (lazy-seq (cons 1 (map f seq-general))))
 
